22606f54faf346fa4de97f54f72b075b3868a87d
Tomasoneill5 <92168523+Tomasoneill5@users.noreply.github.com>
2021-10-08 15:14:50 +0100

    Add files via upload

---
+++ Python exercise1.py
@@ -0,0 +1,49 @@
+
+firstName = input('enter your first name.')
+secondName = input('enter your second name.')
+print('welcome', firstName, secondName)
+
+age = int(input('enter your age.'))
+ageNextYear = (age + 1)
+print('welcome',firstName,secondName,'you will be',ageNextYear,'one year from now')
+
+height = input('enter your height.')
+print('welcome' ,firstName, secondName,'you are', height,'in height', 'you will be', ageNextYear, 'one year from now')
+
+firstNumber= int(input('enter your first number'))
+secondNumber= int(input('enter your second number'))
+sum=(firstNumber+secondNumber)
+print(sum)
+
+thirdNumber= int(input('enter your third number'))
+fourthNumber= int(input('enter your fourth number'))
+sum=(thirdNumber-fourthNumber)
+print(sum)
+
+fifthNumber= int(input('enter your fifth number'))
+sixthNumber= int(input('enter your sixth number'))
+sum=(fifthNumber*sixthNumber)
+print(sum)
+
+seventhNumber= int(input('enter your seventh number'))
+eighthNumber= int(input('enter your eighth number'))
+sum=(seventhNumber/eighthNumber)
+print(sum)
+
+ninthNumber= int(input('enter your ninth number'))
+tenthNumber=int(input('enter your tenth number'))
+sum=(ninthNumber//tenthNumber)
+print(sum)
+
+eleventhNumber= int(input('enter your eleventh number'))
+twelfthNumber= int(input('enter your twelfth number'))
+sum=(eleventhNumber%twelfthNumber)
+print(sum)
+'''
+thirteenthNumber= int(input('enter your thirteenth number'))
+fourteenthNumber= int(input('enter your fourteenth number'))
+fifteenthNumber= int(input('enter your fifteenth number'))
+sum=((int(thirteenthNumber)+ int(fourteenthNumber)+ int(fifteenthNumber))/3)
+print(sum)
+
+
